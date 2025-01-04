981696d8a63c12bd672d7eb0e4ca7bebe65d1773
tz1112 <marcel.antzek@googlemail.com>
2025-01-04 22:46:26 +0000

    Added postCreateCommands.sh in a working configuration

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip3 install -r requirements.txt
+pip3 install -r ./.devcontainer/requirements.txt
 git config filter.strip-notebook-output.clean 'jupyter nbconvert --ClearOutputPreprocessor.enabled=True --to=notebook --stdin --stdout --log-level=ERROR'
 echo "[DONE] Ran postCreateCommand.sh"
